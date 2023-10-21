296c0d6122aee315e767da8be1da9733ccb3e802
BruceBM <bruce_mercado@dlsu.edu.ph>
2023-10-21 08:37:25 +0800

    Copied Mon_view recycler view

--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/CourseViewActivity.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/CourseViewActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CourseViewActivity: AppCompatActivity() {
+    var characterList: ArrayList<Course> = CourseGenerator.generateData()
+
+    private lateinit var recyclerView: RecyclerView
+
     private lateinit var addCourseButton: Button
     private lateinit var scheduleButton: Button
     private lateinit var homeButton: Button
@@ -14,8 +20,22 @@ class CourseViewActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.course_view)
 
+        var context = this
+        var db = DataBaseHandler(context)
+        var data = db.readData()
+
+        var i = 0
+        for (i in 0..(data.size)-1) characterList.add(Course(data.get(i).courseCode,data.get(i).courseSection,data.get(i).courseRoom,data.get(i).courseTime,data.get(i).courseDay))
+
+        this.recyclerView = findViewById(R.id.CV_RecyclerView)
+
+        this.recyclerView.adapter = Adapter(this.characterList, "MONDAY")
+
+        this.recyclerView.layoutManager = LinearLayoutManager(this)
+
         initializeUI()
         initializeListener()
+
     }
 
     private fun initializeUI() {
