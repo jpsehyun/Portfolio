5fe8305b247d3614a2f9529557cd593f5a71cd8d
BruceBM <bruce_mercado@dlsu.edu.ph>
2023-10-21 12:00:17 +0800

    course_list_layout + adapter fix
    Passable course_list_layout and fixed a previous adapter problem

--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/Adapter.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/Adapter.kt
@@ -13,9 +13,6 @@ class Adapter(private val data: ArrayList<Course>, cur_Day: String): Adapter<Vie
 
         val inflater = LayoutInflater.from(parent.context)
         // Inflate a new View given the item_layout.xml item view we created.
-        val view = inflater.inflate(R.layout.course_layout, parent, false)
-        // Return a new instance of our MyViewHolder passing the View object we created
-        return ViewHolder(view, day)
 
         if (day.equals("ALL")){
             //Try out your layout here
