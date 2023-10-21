d6ace87b24405a42341efc713ea03eec209859ad
jpsehyun <jpsehyun@gmail.com>
2023-10-21 09:38:30 +0800

    Adapter to accept template for Course_View

--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/Adapter.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/Adapter.kt
@@ -1,17 +1,23 @@
 package com.example.machineproject_mobdeve_mercado_sillona_park
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
 class Adapter(private val data: ArrayList<Course>, cur_Day: String): Adapter<ViewHolder>() {
     var day:String = cur_Day
+    lateinit var view: View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // Create a LayoutInflater using the parent's (i.e. RecyclerView's) context
+
         val inflater = LayoutInflater.from(parent.context)
-        // Inflate a new View given the item_layout.xml item view we created.
-        val view = inflater.inflate(R.layout.course_layout, parent, false)
-        // Return a new instance of our MyViewHolder passing the View object we created
+
+        if (day.equals("ALL"))
+            //Try out your layout here
+            view = inflater.inflate(R.layout.course_layout, parent, false)
+        else
+            view = inflater.inflate(R.layout.course_layout, parent, false)
+
         return ViewHolder(view, day)
 
     }
