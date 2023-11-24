46b3ac1ccf3fb7469133f8d6cc66a1472974e79f
BruceBM <61485567+BruceBM@users.noreply.github.com>
2023-11-24 11:36:06 +0800

    Incomplete Delete Function
    Adapter does not work on Schedule pages
    
    + format changes

--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/Adapter.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/Adapter.kt
@@ -4,37 +4,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView
 
-class Adapter(private val data: ArrayList<Course>, cur_Day: String): Adapter<ViewHolder>() {
+class Adapter(private val data: ArrayList<Course>, cur_Day: String): RecyclerView.Adapter<ViewHolder>() {
     var day:String = cur_Day
     lateinit var view: View
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // Create a LayoutInflater using the parent's (i.e. RecyclerView's) context
-
         val inflater = LayoutInflater.from(parent.context)
-        // Inflate a new View given the item_layout.xml item view we created.
 
-        if (day.equals("ALL")){
-            //Try out your layout here
+        // Choose the layout based on the current day
+        if (day == "ALL") {
+            // Try out your layout here
             view = inflater.inflate(R.layout.course_list_layout, parent, false)
-
-            //I think the Viewholder might crash with redundant id
-            //So make a separate Viewholder for new course view layout
-            return ViewHolder(view, day)
-        }
-
-        else{
+            // Make a separate ViewHolder for the new course view layout
+            return ViewHolder(view, day, this::onItemDeleteClick)
+        } else {
             view = inflater.inflate(R.layout.course_layout, parent, false)
-            return ViewHolder(view, day)
+            return ViewHolder(view, day, this::onItemDeleteClick)
         }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindData(data.get(position))
+        holder.bindData(data[position])
     }
 
     override fun getItemCount(): Int {
         // This needs to be modified, so don't forget to add this in.
         return data.size
+    }
+
+    private fun onItemDeleteClick(position: Int) {
+        // Remove the item from the list
+        if (position != RecyclerView.NO_POSITION) {
+            data.removeAt(position)
+            // Notify the adapter that the data set has changed
+            notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/ViewHolder.kt
+++ app/src/main/java/com/example/machineproject_mobdeve_mercado_sillona_park/ViewHolder.kt
@@ -2,21 +2,22 @@ package com.example.machineproject_mobdeve_mercado_sillona_park
 
 import android.content.Intent
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class ViewHolder(itemView: View, cur_Day: String): ViewHolder(itemView) {
+class ViewHolder(itemView: View, cur_Day: String, private val onDeleteClick: (Int) -> Unit) : RecyclerView.ViewHolder(itemView) {
     private var courseTime: TextView = itemView.findViewById(R.id.courseTimeTv)
     private var courseCode: TextView = itemView.findViewById(R.id.courseCodeTv)
     private var courseRoom: TextView = itemView.findViewById(R.id.courseRoomTv)
     private var courseSection: TextView = itemView.findViewById(R.id.courseSectionTv)
+    private var deleteButton: ImageButton = itemView.findViewById(R.id.courseDelete_Btn)
 
     lateinit var courseDay: String
     var current_Day = cur_Day
 
     init{
+        // Set up the click listener to navigate to UpdateViewActivity
         itemView.setOnClickListener{v: View ->
             var intentLoadNextActivity = Intent(itemView.context, UpdateViewActivity::class.java)
             intentLoadNextActivity.putExtra("code", courseCode.text.toString())
@@ -27,26 +28,28 @@ class ViewHolder(itemView: View, cur_Day: String): ViewHolder(itemView) {
             itemView.context.startActivity(intentLoadNextActivity)
         }
 
+        // Set click listener for the delete button
+        deleteButton.setOnClickListener {
+            onDeleteClick(adapterPosition)
+        }
     }
 
     fun bindData(course: Course) {
-
+        // Bind data to the views
         courseTime.text = course.courseTime
         courseCode.text = course.courseCode
         courseRoom.text = course.courseRoom
         courseSection.text = course.courseSection
         courseDay = course.courseDay
 
-        itemView.setVisibility(View.VISIBLE)
+        // Set the visibility of the itemView based on the current day
+        itemView.visibility = View.VISIBLE
 
-        if ((courseDay.equals(current_Day) || current_Day.equals("ALL"))){
-            itemView.setVisibility(View.VISIBLE)
-        }
-
-        else{
-            itemView.setVisibility(View.GONE)
-            itemView.setLayoutParams(RecyclerView.LayoutParams(0, 0))
+        if ((courseDay == current_Day) || (current_Day == "ALL")) {
+            itemView.visibility = View.VISIBLE
+        } else {
+            itemView.visibility = View.GONE
+            itemView.layoutParams = RecyclerView.LayoutParams(0, 0)
         }
     }
-
 }
